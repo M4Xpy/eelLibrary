f4d0b5252617c67fde47e91d7d9dd0615c467248
Maxim V <m4k51m15k4m@gmail.com>
2024-04-13 12:06:32 +0300

    fix Dockerfile and main.py

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,30 @@
+# Use an official Python runtime as a parent image
 FROM python:3.10.12-slim
 LABEL authors="voytkevychus@gmail.com"
 
+# Set environment variables
 ENV PYTHONUNBUFFERED=1
 
+# Install system dependencies, including Chromium
+RUN apt-get update \
+    && apt-get install -y --no-install-recommends \
+        chromium \
+        chromium-driver \
+    && rm -rf /var/lib/apt/lists/*
 
-RUN apt-get update && apt-get install -y chromium
+# Set the working directory in the container
+WORKDIR /app
 
-WORKDIR app/
+# Copy the current directory contents into the container at /app
+COPY . /app
 
-COPY requirements.txt requirements.txt
-RUN pip install -r requirements.txt
+# Install any needed dependencies specified in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
-COPY . .
+# Expose the port that your Eel app will run on
+EXPOSE 8000
 
+# Define the command to run your Eel app when the container starts
 CMD ["python", "main.py"]
+
 
--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import eel
 
 # Connection to SQLite database
 conn = sqlite3.connect("books.db")
-c = conn.cursor()
+db_cursor = conn.cursor()
 
 # Create the table if it does not already exist
-c.execute(
+db_cursor.execute(
     """CREATE TABLE IF NOT EXISTS books
              (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT)"""
 )
@@ -20,7 +20,7 @@ eel.init("static")
 @eel.expose
 def create_book(title: str, description: str, ) -> str:
     """ Inserting a new entry into the database """
-    c.execute(
+    db_cursor.execute(
         "INSERT INTO books (title, description) VALUES (?, ?)",
         (title, description)
     )
@@ -31,20 +31,23 @@ def create_book(title: str, description: str, ) -> str:
 @eel.expose
 def get_books() -> list[tuple[int, str, str]]:
     """  Getting all the books from the database """
-    c.execute("SELECT * FROM books")
-    books = c.fetchall()
+    db_cursor.execute("SELECT * FROM books")
+    books = db_cursor.fetchall()
     return books
 
 
 @eel.expose
 def delete_book(book_id: int, ) -> str:
     """ Deleting a book from the database by its ID """
-    c.execute("DELETE FROM books WHERE id=?", (book_id,))
+    db_cursor.execute("DELETE FROM books WHERE id=?",
+                      (book_id,)
+                      )
     conn.commit()
     return "Книга успішно видалена!"
 
 
 if __name__ == '__main__':
-    eel.start(
-        "create_book.html",
-    )
+    eel.start("books_list.html",
+              host='0.0.0.0',
+              port=8000,
+              )
